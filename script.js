f1047a96082b091becbb1234fa20aa402fdb7cd9
nicplackle <nicplackle@gmail.com>
2019-06-26 13:33:14 +0200

    add reset button. but still too much test clutter

--- script.js
+++ script.js
@@ -2,7 +2,31 @@ const cards = document.querySelectorAll(".memory-card");
 
 let hasFlippedCard = false;
 let lockBoard = false;
-let firstCard, secondCard;
+let firstCard;
+let secondCard;
+
+// stars test
+// Access the <ul> element for the star rating section and then the <li> elements within it
+const star = document.getElementById("star-rating").querySelectorAll(".star");
+// Variable to keep track of how many stars are left
+let starCount = 3;
+// end stars test
+
+// moves test
+// Select the class moves-counter and change it's HTML
+const movesCount = document.querySelector(".moves-counter");
+// Create variable for moves counter, start the count at zero
+let moves = 0;
+// end moves test
+
+// Access the reset button
+const reset = document.querySelector(".reset-btn");
+
+// reset
+document.getElementById("reset").onclick = function() {
+  window.location.reload();
+};
+// reset
 
 function flipCard() {
   if (lockBoard) return;
@@ -46,6 +70,14 @@ function unflipCards() {
 
     resetBoard();
   }, 1500);
+
+  // moves counter test
+  movesCounter();
+  // end moves counter test
+
+  // stars test
+  starRating();
+  // end stars test
 }
 
 function resetBoard() {
@@ -59,72 +91,51 @@ function resetBoard() {
     card.style.order = randomPos;
   });
 })();
+/*
+// reset test
+function resetEverything() {
+  // Stop time, reset the minutes and seconds update the time inner HTML
+  stopTime();
+  timeStart = false;
+  seconds = 0;
+  minutes = 0;
+  timeCounter.innerHTML =
+    "<i class='fa fa-hourglass-start'></i>" + " Timer: 00:00";
+  // Reset star count and the add the class back to show stars again
+  star[1].firstElementChild.classList.add("fa-star");
+  star[2].firstElementChild.classList.add("fa-star");
+  starCount = 3;
+  // Reset moves count and reset its inner HTML
+  moves = 0;
+  movesCount.innerHTML = 0;
+  // Clear both arrays that hold the opened and matched cards
+  matched = [];
+  opened = [];
+  // Clear the deck
+  removeCard();
+  // Create a new deck
+  startGame();
+}
+// end reset test
+*/
 
-cards.forEach(card => card.addEventListener("click", flipCard));
-
-/* const cards = document.querySelectorAll(".memory-card");
-
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
-
-function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
-
-  this.classList.add("flip");
-  // first click
-  if (!hasFlippedCard) {
-    hasFlippedCard = true;
-    firstCard = this;
-  } else {
-    // second click
-    hasFlippedCard = false;
-    secondCard = this;
-
-    checkForMatch();
+// stars test
+/*
+Update the star rating.  Depending on the number of
+moves the user completes the game, the stars will decrease
+with the more moves the user takes.
+*/
+function starRating() {
+  if (moves === 8) {
+    // First element child is the <i> within the <li>
+    star[2].firstElementChild.classList.remove("fa-star");
+    starCount--;
+  }
+  if (moves === 12) {
+    star[1].firstElementChild.classList.remove("fa-star");
+    starCount--;
   }
 }
+// stars test
 
-function checkForMatch() {
-  // card match?
-  if (firstCard.dataset.framework === secondCard.dataset.framework) {
-    //  we have a match!
-    disableCards();
-  } else {
-    // no match
-    unflipCards();
-  }
-
-  function disableCards() {
-    firstCard.removeEventlistener("click", flipCard);
-    secondCard.removeEventListener("click", flipCard);
-
-    resetBoard();
-  }
-
-  function unflipCards() {
-    lockBoard = true;
-
-    setTimeout(() => {
-      firstCard.classlist.remove("flip");
-      secondCard.classlist.remove("flip");
-
-      lockBoard = false;
-    }, 1500);
-  }
-
-  function resetBoard() {
-    [hasFlippedCard, lockBoard] = [false, false];
-    [firstCard, secondCard] = [null, null];
-  }
-
-  (function shuffle() {
-    cards.forEach(card => {
-      let randomPos = Math.floor(Math.random() * 12);
-      card.style.order = randomPos;
-    });
-  })();
-
-  cards.forEach(card => card.addEventListener("click", flipCard));
-} */
+cards.forEach(card => card.addEventListener("click", flipCard));
