fea4482b9a216a3b72fd7fe42706f154f9d45fc9
nicplackle <nicplackle@gmail.com>
2019-06-27 09:39:31 +0200

    add non working timer to page

--- script.js
+++ script.js
@@ -5,12 +5,10 @@ let lockBoard = false;
 let firstCard;
 let secondCard;
 
-// stars test
 // Access the <ul> element for the star rating section and then the <li> elements within it
-const star = document.getElementById("star-rating").querySelectorAll(".star");
+// const star = document.getElementById("star-rating").querySelectorAll(".star");
 // Variable to keep track of how many stars are left
-let starCount = 3;
-// end stars test
+// let starCount = 3;
 
 // Select the class moves-counter and change it's HTML
 const movesCount = document.querySelector(".moves-counter");
@@ -21,11 +19,17 @@ let moves = 0;
 // Access the reset button
 const reset = document.querySelector(".reset-btn");
 
+// span tag for the timer
+const timeCounter = document.querySelector(".timer");
+let time;
+let minutes = 0;
+let seconds = 0;
+let timeStart = false;
+
 // reset
 document.getElementById("reset").onclick = function() {
   window.location.reload();
 };
-// reset
 
 function flipCard() {
   if (lockBoard) return;
@@ -74,7 +78,7 @@ function unflipCards() {
   movesCounter();
 
   // stars test
-  starRating();
+  // starRating();
   // end stars test
 }
 
@@ -102,7 +106,7 @@ Update the star rating.  Depending on the number of
 moves the user completes the game, the stars will decrease
 with the more moves the user takes.
 */
-function starRating() {
+/*function starRating() {
   if (moves === 8) {
     // First element child is the <i> within the <li>
     star[2].firstElementChild.classList.remove("fa-star");
@@ -112,7 +116,43 @@ function starRating() {
     star[1].firstElementChild.classList.remove("fa-star");
     starCount--;
   }
-}
+}*/
 // stars test
+
+// timer test
+function timer() {
+  // Update the count every 1 second
+  time = setInterval(function() {
+    seconds++;
+    if (seconds === 60) {
+      minutes++;
+      seconds = 0;
+    }
+    // Update the timer in HTML with the time it takes the user to play the game
+    timeCounter.innerHTML =
+      "<i class='fa fa-clock'></i>" +
+      " Timer: " +
+      minutes +
+      " Mins " +
+      seconds +
+      " Secs";
+  }, 1000);
+}
+
+function stopTime() {
+  clearInterval(time);
+}
+
+// end timer test
+
+// resest everything test
+// end reset everything test
+/*function resetEverything() {
+  stopTime();
+  timeStart = false;
+  seconds = 0;
+  minutes = 0;
+  timeCounter.innerHTML = "<i class='fa fa-clock'></i>" + " Timer: 00:00";
+}*/
 
 cards.forEach(card => card.addEventListener("click", flipCard));
